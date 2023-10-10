53811fb34277d3e1302b2a97d4b42f8a7651c892
Eric Russo <erickfernusg1@gmail.com>
2023-10-10 16:11:40 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app= express();
 
 app.use(cors({
 
-    //origin: 'http://localhost:5173',
-    origin: FRONTEND_URL,
+    origin: 'http://localhost:5173',
+   // origin: FRONTEND_URL,
     credentials: true,
 })
     
